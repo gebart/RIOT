d44432478896104600b13182becc07e8dfa28b52
Joakim Gebart <joakim@gebart.se>
2014-09-10 20:40:23 +0200

    WIP hwtimers

--- cpu/k60/hwtimer_arch.c
+++ cpu/k60/hwtimer_arch.c
@@ -24,6 +24,10 @@
 #include "thread.h"
 #include "hwtimer_cpu.h"
 
+#if HW_TIMERS_COUNT > HWTIMER_MAXTIMERS
+#error HW_TIMERS_COUNT (board.h) > HWTIMER_MAXTIMERS (hwtimer_cpu.h)
+#endif
+
 /**
  * @brief Callback function that is given to the low-level timer
  *
@@ -40,45 +44,55 @@ void hwtimer_arch_init(void (*handler)(int), uint32_t fcpu)
 {
     timeout_handler = handler;
     /* Initialize all channels that have been designated for HW timer library use. */
-    for(int i = 0; i < HW_TIMER_COUNT; ++i)
+    for(int i = 0; i < HW_TIMERS_COUNT; ++i)
     {
-        timer_init(i, HWTIMER_SPEED/1000000, &irq_handler);
+        timer_init(HW_TIMER + i, HWTIMER_SPEED/1000000, &irq_handler);
     }
 }
 
 void hwtimer_arch_enable_interrupt(void)
 {
-    DEBUG("hwtimer_arch_enable_interrupt(): not implemented\n");
-    /* timer_irq_enable(HW_TIMER); */
+    /* Turn on interrupts on all channels, this is probably not desired... */
+    for(int i = 0; i < HW_TIMERS_COUNT; ++i)
+    {
+        timer_irq_enable(HW_TIMER + 1 + i);
+    }
 }
 
 void hwtimer_arch_disable_interrupt(void)
 {
-    DEBUG("hwtimer_arch_disable_interrupt(): not implemented\n");
-    /* timer_irq_disable(HW_TIMER); */
+    /* Turn off interrupts on all channels, this is probably not desired... */
+    for(int i = 0; i < HW_TIMERS_COUNT; ++i)
+    {
+        timer_irq_disable(HW_TIMER + 1 + i);
+    }
 }
 
 void hwtimer_arch_set(unsigned long offset, short timer)
 {
-    timer_set(timer, 0, offset);
+    timer_set(HW_TIMER + 1 + timer, 0, offset);
 }
 
 void hwtimer_arch_set_absolute(unsigned long value, short timer)
 {
-    timer_set_absolute(timer, 0, value);
+    /* We have to count backwards from the absolute time reference in order to
+     * know what delay to set. */
+    timer_set(HW_TIMER + 1 + timer, 0, value - hwtimer_arch_now());
 }
 
 void hwtimer_arch_unset(short timer)
 {
-    timer_clear(timer, 0);
+    timer_clear(HW_TIMER + 1 + timer, 0);
 }
 
 unsigned long hwtimer_arch_now(void)
 {
+    /* Read the absolute reference timer */
     return timer_read(HW_TIMER);
 }
 
 void irq_handler(int channel)
 {
-    timeout_handler((short)(channel));
+    /* Translate back to hwtimer library timer numbers from physical hardware channel index */
+    timeout_handler((short)(channel - HW_TIMER));
 }
