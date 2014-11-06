8384c42322fb8414144e847bc200a632cd83bb60
Hauke Petersen <hauke.petersen@fu-berlin.de>
2014-11-06 17:17:24 +0100

    tests: added test for tim

---
+++ tests/tim/main.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup tests
+ * @{
+ *
+ * @file
+ * @brief       Test application for testing tim
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+
+#include "tim.h"
+
+
+int main(void)
+{
+    puts("tim test application...");
+
+
+
+    return 0;
+}
