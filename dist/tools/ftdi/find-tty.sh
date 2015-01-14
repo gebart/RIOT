140f31f9c32bf0d036fc7daf772c27164962ea5c
Joakim Gebart <joakim.gebart@eistec.se>
2015-01-14 15:59:45 +0100

    ftdi: Suppress error messages and by default match any serial.

--- dist/tools/ftdi/find-tty.sh
+++ dist/tools/ftdi/find-tty.sh
@@ -8,34 +8,33 @@
 # details.
 #
 
-if [ $# -lt 1 ]; then
-    cat <<EOF
-Usage: $(basename "${0}") [serial]
- The first tty device node assigned to the USB device with the given serial number is written to stdout.
-EOF
-    exit 1
-fi
-
 # Find all USB to serial devices
 for dev in /sys/bus/usb/devices/*; do
     if [ ! -f "${dev}/idVendor" ]; then
         # not a main device
         continue
     fi
-    serial=$(cat "${dev}/serial")
+    # Try to read the device serial, discard any error messages (2>/dev/null)
+    serial=$(cat "${dev}/serial" 2>/dev/null)
     # Look if any subdevices have a tty directory, this means that it is assigned a port.
     unset ttys
-    ttys=$(ls "${dev}:"*/tty* -d -1 | xargs -n 1 basename)
+    ttys=$( (ls "${dev}:"*/tty* -d -1 2>/dev/null | xargs -n 1 basename) 2>/dev/null)
     if [ -z "${ttys}" ]; then
         continue
     fi
     # split results into array
     read -r -a ttys <<< ${ttys}
+    if [ $# -lt 1 ]; then
+        # No arguments given, return first found tty
+        echo "/dev/${ttys[0]}"
+        exit 0
+    fi
+    # else: Match any of the given serials
     for s in "${@}"; do
         if [ "${serial}" == "${s}" ]; then
             # return first tty
             echo "/dev/${ttys[0]}"
-            exit 0;
+            exit 0
         fi
     done
 done
