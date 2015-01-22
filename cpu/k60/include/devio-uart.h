5dc2a875d4b66db7be9685548dabfad6464f28be
Joakim Gebart <joakim.gebart@eistec.se>
2015-01-22 18:02:28 +0100

    k60: Add extern "C" to headers.

--- cpu/k60/include/devio-uart.h
+++ cpu/k60/include/devio-uart.h
@@ -20,12 +20,21 @@
 #ifndef DEVIO_UART_H_
 #define DEVIO_UART_H_
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 long uart0_write_r(struct _reent *r, int fd, const char *ptr, int len);
 long uart1_write_r(struct _reent *r, int fd, const char *ptr, int len);
 long uart2_write_r(struct _reent *r, int fd, const char *ptr, int len);
 long uart3_write_r(struct _reent *r, int fd, const char *ptr, int len);
 long uart4_write_r(struct _reent *r, int fd, const char *ptr, int len);
 long uart0_read_r(struct _reent *r, int fd, char *ptr, int len);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* !defined(DEVIO_UART_H_) */
 /** @} */
