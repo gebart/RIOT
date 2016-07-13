5850497b64eea395207be7606f3defbe4921a7aa
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2016-07-13 12:02:52 +0200

    squash devfs basic directory support

--- sys/fs/devfs/devfs.c
+++ sys/fs/devfs/devfs.c
@@ -39,13 +39,25 @@ static mutex_t _devfs_mutex = MUTEX_INIT;
 static int devfs_open(vfs_file_t *filp, const char *name, int flags, int mode, const char *abs_path);
 static int devfs_fcntl(vfs_file_t *filp, int cmd, int arg);
 
+/* Directory operations */
+static int devfs_opendir(vfs_DIR *dirp, const char *dirname, const char *abs_path);
+static int devfs_readdir(vfs_DIR *dirp, vfs_dirent_t *entry);
+static int devfs_closedir(vfs_DIR *dirp);
+
 static const vfs_file_ops_t devfs_file_ops = {
     .open  = devfs_open,
     .fcntl = devfs_fcntl,
 };
 
+static const vfs_dir_ops_t devfs_dir_ops = {
+    .opendir = devfs_opendir,
+    .readdir = devfs_readdir,
+    .closedir = devfs_closedir,
+};
+
 const vfs_file_system_t devfs_file_system = {
     .f_op = &devfs_file_ops,
+    .d_op = &devfs_dir_ops,
 };
 
 static int devfs_open(vfs_file_t *filp, const char *name, int flags, int mode, const char *abs_path)
@@ -87,6 +99,52 @@ static int devfs_fcntl(vfs_file_t *filp, int cmd, int arg)
         default:
             return -EINVAL;
     }
+}
+
+static int devfs_opendir(vfs_DIR *dirp, const char *dirname, const char *abs_path)
+{
+    DEBUG("devfs_opendir: %p, \"%s\", \"%s\"\n", (void *)dirp, dirname, abs_path);
+    if (strncmp(dirname, "/", 2) != 0) {
+        /* We keep it simple and only support a flat file system, only a root directory */
+        return -ENOENT;
+    }
+    dirp->private_data.ptr = _devfs_head;
+    return 0;
+}
+
+static int devfs_readdir(vfs_DIR *dirp, vfs_dirent_t *entry)
+{
+    DEBUG("devfs_readdir: %p, %p\n", (void *)dirp, (void *)entry);
+    devfs_t *node = dirp->private_data.ptr;
+    if (node == NULL) {
+        /* end of list */
+        return 0;
+    }
+    if (node->path == NULL) {
+        return -EIO;
+    }
+    size_t len = strnlen(node->path, VFS_NAME_MAX + 1);
+    if (len > VFS_NAME_MAX) {
+        /* name does not fit in vfs_dirent_t buffer */
+        /* skipping past the broken entry */
+        node = node->next;
+        dirp->private_data.ptr = node;
+        return -EAGAIN;
+    }
+    /* copy the string, including terminating null */
+    memcpy(&entry->d_name[0], node->path, len + 1);
+    node = node->next;
+    dirp->private_data.ptr = node;
+    return 1;
+}
+
+static int devfs_closedir(vfs_DIR *dirp)
+{
+    /* Just an example, it's not necessary to define closedir if there is
+     * nothing to clean up */
+    (void) dirp;
+    DEBUG("devfs_closedir: %p\n", (void *)dirp);
+    return 0;
 }
 
 int devfs_register(devfs_t *node)
