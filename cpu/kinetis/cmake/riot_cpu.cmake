3068a396253b586847a2784b766899a36a97feb5
Joakim Nohlgård <jnohlgard@spotify.com>
2021-02-17 15:09:45 +0100

    WIP cmake

--- cpu/kinetis/cmake/riot_cpu.cmake
+++ cpu/kinetis/cmake/riot_cpu.cmake
@@ -1,6 +1,3 @@
 if(NOT CPU)
   message(FATAL_ERROR "CPU is not defined")
 endif()
-set(RIOT_CPU_DIR "${RIOT_CPU_BASEDIR}/${CPU}"
-set(LINKER_SCRIPT "kinetis.ld")
-add_link_options(${CPU_C_FLAGS} -T ${LINKER_SCRIPT} "-L${RIOT_CPU_DIR}/ldscripts")
