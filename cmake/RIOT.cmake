36bec47f8244b746ebbcda1f7bae4ccd3a850be5
Joakim Nohlgård <jnohlgard@spotify.com>
2021-02-17 15:09:45 +0100

    WIP cmake

--- cmake/RIOT.cmake
+++ cmake/RIOT.cmake
@@ -1,17 +1,18 @@
 # This internal cache variable is required to pass the list of object library files to the top level CMakeLists
 set(RIOT_STARTFILES "" CACHE INTERNAL "Linker dependencies that must be explicitly linked with any executable")
 
-# Path configuration
-set(RIOT_BASEDIR ${CMAKE_CURRENT_LIST_DIR} CACHE PATH "Path to the RIOT source tree root")
+## Path configuration
+set(RIOT_BASEDIR ${CMAKE_CURRENT_LIST_DIR}/.. CACHE PATH "Path to the RIOT source tree root")
 set(RIOT_CPU_BASEDIR ${RIOT_BASEDIR}/cpu CACHE PATH "Path to cpu component of the RIOT tree")
-set(RIOT_BOARD_BASEDIR ${RIOT_BASEDIR}/board CACHE PATH "Path to board component of the RIOT tree")
+set(RIOT_BOARD_BASEDIR ${RIOT_BASEDIR}/boards CACHE PATH "Path to board component of the RIOT tree")
 set(RIOT_BOARD_DIR ${RIOT_BOARD_BASEDIR}/${BOARD} CACHE PATH "Path to board specific code")
+list(APPEND CMAKE_MODULE_PATH "${RIOT_BASEDIR}/cmake")
 
 option(RIOT_WERROR "Build with all warnings as errors" ON)
 
 # Use riot_configure_target to set all required linker options and libraries for building an executable
 macro(riot_configure_target name)
-  target_link_libraries(${name} PRIVATE RIOT ${DERI_STARTFILES})
+  target_link_libraries(${name} PRIVATE RIOT ${RIOT_STARTFILES})
   if (LINKER_SCRIPT)
     set_target_properties(${name} PROPERTIES LINK_DEPENDS ${LINKER_SCRIPT})
   endif ()
@@ -21,12 +22,18 @@ macro(riot_configure_target name)
   endif ()
 endmacro()
 
+if (NOT BOARD STREQUAL native)
+  set(CMAKE_EXECUTABLE_SUFFIX .elf)
+endif ()
+
+
 # Load board specific CMake settings
-list(APPEND CMAKE_MODULE_PATH
-  "${RIOT_BOARD_DIR}/cmake"
-  "${RIOT_CPU_DIR}/cmake"
-  "${RIOT_BASEDIR}/cmake")
+list(APPEND CMAKE_MODULE_PATH "${RIOT_BOARD_DIR}/cmake")
 include(riot_board)
+
+set(RIOT_CPU_DIR ${RIOT_CPU_BASEDIR}/${CPU} CACHE PATH "Path to CPU specific code")
+list(APPEND CMAKE_MODULE_PATH "${RIOT_CPU_DIR}/cmake")
 include(riot_cpu)
-include(riot_common_options)
+
+add_subdirectory(${RIOT_BASEDIR} ${CMAKE_CURRENT_BINARY_DIR}/RIOT)
 
