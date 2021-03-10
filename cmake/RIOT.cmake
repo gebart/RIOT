fb6971fe898d8c4f4a529b40e7d598a66a610955
Joakim Nohlgård <jnohlgard@spotify.com>
2021-03-10 10:06:44 +0100

    cmake: Use INTERFACE_SOURCES for modules

--- cmake/RIOT.cmake
+++ cmake/RIOT.cmake
@@ -1,8 +1,3 @@
-## Path configuration
-get_filename_component(RIOT_BASEDIR "${CMAKE_CURRENT_LIST_DIR}" DIRECTORY CACHE)
-set(RIOT_BASEDIR "${RIOT_BASEDIR}" CACHE PATH "Path to the RIOT source tree root")
-list(APPEND CMAKE_MODULE_PATH "${RIOT_BASEDIR}/cmake")
-
 # Use riot_configure_target to set all required linker options and libraries for building an executable
 macro(riot_configure_target name)
   target_link_libraries(${name} PRIVATE RIOT ${RIOT_STARTFILES})
@@ -14,23 +9,3 @@ macro(riot_configure_target name)
     target_link_options(${name} PRIVATE LINKER:-Map=$<TARGET_FILE_BASE_NAME:${name}>.map)
   endif ()
 endmacro()
-
-if (NOT BOARD STREQUAL native)
-  set(CMAKE_EXECUTABLE_SUFFIX .elf)
-endif ()
-
-# Load board specific CMake settings
-set(RIOT_BOARD_DIR "${RIOT_BASEDIR}/boards/${BOARD}" CACHE PATH "Path to the board-specific code")
-list(APPEND CMAKE_MODULE_PATH "${RIOT_BOARD_DIR}/cmake")
-include(riot_board)
-
-# riot_board.cmake above should set ${CPU}
-set(RIOT_CPU_DIR "${RIOT_BASEDIR}/cpu/${CPU}" CACHE PATH "Path to the CPU-specific code")
-list(APPEND CMAKE_MODULE_PATH "${RIOT_CPU_DIR}/cmake")
-include(riot_cpu)
-
-if (NOT MCU)
-  set(MCU "${CPU}")
-endif()
-add_subdirectory("${RIOT_BASEDIR}" "${CMAKE_CURRENT_BINARY_DIR}/RIOT")
-
--- cmake/riot_config.cmake
+++ cmake/riot_config.cmake
@@ -2,11 +2,29 @@ include_guard(GLOBAL)
 # Global configuration variables that can be assumed to be available in all
 # RIOT project configurations
 
+# Load board specific CMake settings
+set(RIOT_BOARD_DIR "${PROJECT_SOURCE_DIR}/boards/${BOARD}" CACHE PATH "Path to the board-specific code")
+list(PREPEND CMAKE_MODULE_PATH "${RIOT_BOARD_DIR}/cmake")
+include(riot_board)
+
+# riot_board.cmake above should set ${CPU}
+set(RIOT_CPU_DIR "${PROJECT_SOURCE_DIR}/cpu/${CPU}" CACHE PATH "Path to the CPU-specific code")
+list(PREPEND CMAKE_MODULE_PATH "${RIOT_CPU_DIR}/cmake")
+include(riot_cpu)
+
+if (NOT MCU)
+  set(MCU "${CPU}")
+endif()
+
+if (NOT BOARD STREQUAL native)
+  set(CMAKE_EXECUTABLE_SUFFIX .elf)
+endif ()
+
 option(RIOT_DEVELHELP "Enable helpful developer settings. This will add some extra checks which indicate programmer mistakes and add some extra debug output in various places." OFF)
 
 option(RIOT_WERROR "Build with all warnings as errors" OFF)
 
-set(RIOT_USEMODULE_EXTRA "" CACHE STRING "Additional modules to enable in this build")
+set(RIOT_USEMODULE_EXTRA "" CACHE STRING "Additional modules to enable globally for all targets in this build")
 
 set(RIOT_LIBC "newlib" CACHE STRING
   "Specifies which libc implementation to use. If unsure, say \"newlib\"")
@@ -32,4 +50,3 @@ if (RIOT_SYSCALLS MATCHES "^\(.*\)_syscalls_default$")
     set(RIOT_SYSCALLS "${RIOT_LIBC}_syscalls_default" CACHE STRING "${helpstring}" FORCE)
   endif()
 endif()
-
